6f7f9e76ebc71644ac9249832e1996932d7f549c
Yaseen Salah <52181539+yaseensalah@users.noreply.github.com>
2021-06-06 21:05:53 +0200

    Updated the LED Function file
    Adding initialization required for LED function as separate code

--- LED Function/LED_Function.c
+++ LED Function/LED_Function.c
@@ -1,6 +1,19 @@
 #include "stdint.h"
+#include "C:/Users/Dell/Desktop/GPS Project/tm4c123gh6pm.h"
 #define RED 0x02
 #define GREEN 0x08
+void SystemInit(){}
+void Initialize(){
+	SYSCTL_RCGCGPIO_R |= 0x20;
+	while((SYSCTL_PRGPIO_R)==0);
+	GPIO_PORTF_LOCK_R =0x4C4F434B;
+	GPIO_PORTF_CR_R =0x1F; //PINS used in PORT_F
+	GPIO_PORTF_AFSEL_R =0;
+	GPIO_PORTF_AMSEL_R =0;
+	GPIO_PORTF_DIR_R =0x0E; //LEDs Output
+	GPIO_PORTF_DEN_R =0x1F;
+	GPIO_PORTF_PUR_R =0x11;
+}
 
 void LED(float distance) {
 	if( distance >=100){
