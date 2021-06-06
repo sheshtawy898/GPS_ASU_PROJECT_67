cc8ab78b94f5e459fafb2ae13bf7cef21710e2d2
Yaseen Salah <52181539+yaseensalah@users.noreply.github.com>
2021-06-06 20:02:20 +0200

    Fixed bug in LED testing code

--- LED Function/LED function test.c
+++ LED Function/LED function test.c
@@ -15,6 +15,7 @@ void Initialize(){
 	GPIO_PORTF_DEN_R =0x1F;
 	GPIO_PORTF_PUR_R =0x11;
 }
+
 void LED(float distance) {
 	if( distance >=100){
 		GPIO_PORTF_DATA_R = GREEN;
@@ -25,13 +26,12 @@ void LED(float distance) {
 }
 
 int main(){
-
 	float x;
 	Initialize();
 while(1){
-	if((GPIO_PORTF_DATA_R&0x11)==0x01){ x=0;}         //eqv. to start tracking case (Press SW_1)
-	else if((GPIO_PORTF_DATA_R&0x11)==0x00){ x=100;}  //eqv. to reach destination case (Press SW_2)
-	led(x);
+	if((GPIO_PORTF_DATA_R&0x11)==0x01){x=0;}         //eqv. to the default case (Press SW_1 or SW_1&2)
+	else if((GPIO_PORTF_DATA_R&0x11)==0x10){x=100;}  //eqv. to reach destination case (Press SW_2)
+	LED(x);
+}
 }
 
-}
