f3e88fd974ca20fe531a216c1180838e430f4023
Mostafa Elsheshtawy <85258909+sheshtawy898@users.noreply.github.com>
2021-06-06 14:11:54 +0200

    UpdateDistanceFunction_Final.c

--- Distance Function/UpdateDistanceFunction_Test.c
+++ Distance Function/UpdateDistanceFunction_Test.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include<math.h>
+double lat1 =0 , lon1=0 , lat2=0 , lon2=0 ;
+double DISTANCE=0;
+double dist_2p;
+    //GETTING LAT AND LON AFTER CONNECTING GPS//
+ /*double latFromGPS{}
+   double lonFromGPS{} */
 
-       // CONVERTING TO RADIANS //
+   // CONVERTING TO RADIANS //
  double degreesToRadians (double degrees)
 { double pi =3.14;
   return degrees * pi / 180;
 }
-    // GETTING DISTANCE BETWEEN TWO COORDINATES //
+
+   // GETTING DISTANCE BETWEEN TWO COORDINATES //
  double distanceCoordinates (double lat1, double lon1, double lat2, double lon2)
 {
   double earthRadiusKm = 6371;
@@ -23,26 +30,27 @@
   double c = 2 * atan2 (sqrt (a), sqrt (1 - a));
   return earthRadiusKm * c*1000;
 }
-   //FUNCTION TO ACCUMULATE DISTANCE//
-double UpdateDistance (double lat1 , double lon1 , double lat2, double lon2 , double distance){
 
-    if (lat1 != 0 && lon1 != 0)
-    {
-        distance += distanceCoordinates (lat1, lon1, lat2, lon2);
-    }
-    lat1 = lat2;
-    lon1 = lon2;
+
+int main(){
+
+printf("Enter Lat2 then lon2:\n" );  //to try to get the coordinates as if the gps module is connected
+scanf("%lf", &lat2);
+scanf("%lf", &lon2);
+ dist_2p= distanceCoordinates(lat1, lon1 ,lat2, lon2);
+
+
+while (DISTANCE < 100){
+    lat1=lat2;
+    lon1=lon2;
+    printf("Enter Lat2 then lon2:\n" );
+    scanf("%lf", &lat2);
+    scanf("%lf", &lon2);
+    dist_2p= distanceCoordinates(lat1, lon1 ,lat2, lon2);
+    DISTANCE += dist_2p;
 }
-  //TESTING FUNCTIONS//
-int main()
-{
-    double distance =0;
 
-    double lat1=0 ,lat2=30.06421 , lon1=0 , lon2=31.27842;
-    distance += UpdateDistance( lat1 , lon1 , lat2, lon2, distance );
-    printf("%lf\n" , distance);
+printf("%lf", DISTANCE);
 
-    lat2=30.06429 ,lat1=30.06421 , lon2=31.27864 , lon1=31.27842;
-    distance += UpdateDistance( lat1 , lon1 , lat2, lon2, distance );
-    printf("%lf" , distance);
+
 }
