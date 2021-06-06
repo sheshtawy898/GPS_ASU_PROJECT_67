f950aa4fbfa60fd6b4787a6d0c8c18b92bac1f44
Nada Amgad <85258908+NadaAmgadSayed@users.noreply.github.com>
2021-06-06 19:48:48 +0200

    Update Distance_Test.c

--- Distance Function/Distance_Test.c
+++ Distance Function/Distance_Test.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include<math.h>
-
+#define PI 3.14159
+double dLat;
+double dLon;
+double a;
+double c;
       // CONVERTING TO RADIANS //
  double degreesToRadians (double degrees)
-{ double pi =3.14;
-  return degrees * pi / 180;
+{ 
+  return degrees * PI / 180;
 }
 
   // GETTING DISTANCE BETWEEN TWO COORDINATES //
@@ -12,23 +16,23 @@
 {
   double earthRadiusKm = 6371;
 
-  double dLat = degreesToRadians (lat2 - lat1);
-  double dLon = degreesToRadians (lon2 - lon1);
+  dLat = degreesToRadians (lat2 - lat1);
+  dLon = degreesToRadians (lon2 - lon1);
 
   lat1 = degreesToRadians (lat1);
   lat2 = degreesToRadians (lat2);
 
-  double a = sin (dLat / 2) * sin (dLat / 2) +
+   a = sin (dLat / 2) * sin (dLat / 2) +
     sin (dLon / 2) * sin (dLon / 2) * cos (lat1) *
     cos (lat2);
-  double c = 2 * atan2 (sqrt (a), sqrt (1 - a));
+   c = 2 * atan2 (sqrt (a), sqrt (1 - a));
   return earthRadiusKm * c*1000;
 }
 
   //TESTING THE TWO FUNCTIONS//
 int main(){
     double y;
-    y = distanceCoordinates(30.06422,31.27989,30.06372,31.28013);
-    printf("%lf",y);
+    y = distanceCoordinates(30.06422,31.27989,30.06372,31.28013); 
+    printf("%lf",y); //y=60.203997
 
 }
