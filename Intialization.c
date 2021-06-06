80a5f91cb6f09af4ee941279da3ca5458b142dde
MostafaHassan0053 <85255182+MostafaHassan0053@users.noreply.github.com>
2021-06-06 11:15:44 +0200

    initialize port F to enable the led

--- Intialization.c
+++ Intialization.c
@@ -6,7 +6,7 @@ void intialization()
 	//enable the clock for all ports
   SYSCTL_RCGCGPIO_R |= 0x23 ;
 	while((SYSCTL_PRGPIO_R & 0XFF)==0) ;
-	//initialization port A as digital outputs with  
+	//initialization port A as digital outputs  
 	GPIO_PORTA_LOCK_R = 0x4C4F434B ;
 	GPIO_PORTA_CR_R = 0xFF ;
 	GPIO_PORTA_PCTL_R = 0x11 ;
@@ -15,6 +15,7 @@ void intialization()
 	GPIO_PORTA_DEN_R = 0xFF ;
 	GPIO_PORTA_AMSEL_R &= ~ 0xFF ;
 	GPIO_PORTA_DIR_R = 0xFF ;
+	//initialization port B as digital outputs  
 	GPIO_PORTB_LOCK_R = 0x4C4F434B ;
 	GPIO_PORTB_CR_R = 0xFF ;
 	GPIO_PORTB_PCTL_R = 0 ;
@@ -23,12 +24,13 @@ void intialization()
 	GPIO_PORTB_AMSEL_R &= ~ 0xFF ;
 	GPIO_PORTB_AFSEL_R &= ~ 0xFF  ;
 	GPIO_PORTB_DIR_R = 0xFF ;
-	GPIO_PORTF_LOCK_R = 0x4C4F434B ;
-	GPIO_PORTF_CR_R = 0x1E ;
-	GPIO_PORTF_PCTL_R &= ~ 0xFFFFF ;
-  GPIO_PORTF_PUR_R = 0x11 ;
-	GPIO_PORTF_DEN_R = 0x1F ;
-	GPIO_PORTF_AMSEL_R &= ~ 0x1F ;
-	GPIO_PORTF_AFSEL_R &= ~ 0x1F  ;
-	GPIO_PORTF_DIR_R = 0x0E ;
+	//initialization port F as p0,p4 digital inputs and p1,p2,p3 as digital outputs
+	//used to enable the built-in led to green and red 
+	GPIO_PORTF_LOCK_R =0x4C4F434B;
+	GPIO_PORTF_CR_R =0x1F; //PINS used in PORT_F
+	GPIO_PORTF_AFSEL_R =0;
+	GPIO_PORTF_AMSEL_R =0;
+	GPIO_PORTF_DIR_R =0x0E; //LEDs Output
+	GPIO_PORTF_DEN_R =0x1F;
+	GPIO_PORTF_PUR_R =0x11;
 }	
